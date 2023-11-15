cfd2c1c3c116069a30993b53fcbe963e8a2526de
fritz <fritzner.neptune1.ctr@navy.mil>
2023-11-14 23:14:26 -0800

    Add positive and negative tests

---
+++ src/main/java/wordle/Wordle.java
@@ -0,0 +1,19 @@
+package wordle;
+
+import java.util.List;
+import static wordle.Match.*;
+
+enum Match {
+	EXACT, NO
+}
+
+public class Wordle {
+
+	public static List<Match> tally(String target, String guess) {
+		if(guess.equals(target)) {
+			return List.of(EXACT,EXACT,EXACT,EXACT,EXACT);
+		}
+		
+		return List.of(NO,NO,NO,NO,NO);		
+	}
+}
--- src/test/java/wordle/WordleTest.java
+++ src/test/java/wordle/WordleTest.java
@@ -1,14 +1,29 @@
 package wordle;
 
 import static org.junit.Assert.*;
-
+import static wordle.Wordle.tally;
+import static wordle.Match.*;
+import java.util.List;
 import org.junit.Test;
 
 public class WordleTest {
-
+	
 	@Test
 	public void canary() {		
 		assertTrue(true);
 	}
-
+	
+	@Test
+	public void tallyForTargetFAVORAndGuessFAVOR() {		
+		List<Match> response =tally("FAVOR", "FAVOR");
+		
+		assertEquals(List.of(EXACT,EXACT,EXACT, EXACT, EXACT), response);
+	}
+	
+	@Test
+	public void tallyForTargetFAVORAndGuessTESTS() {		
+		List<Match> response =tally("FAVOR", "TESTS");
+		
+		assertEquals(List.of(NO,NO,NO,NO,NO), response);
+	}
 }
